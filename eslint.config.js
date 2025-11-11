fcb4ded5de3e3b895a3d800afb2d85e076077e06
Ram Pal <ram.pal1308@gmail.com>
2025-11-11 12:06:04 +0530

    add lint, prettier, husky to the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint, { plugin } from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +17,28 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
     ],
+    plugins: {
+      reactRefresh,
+      reactPlugin,
+      plugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': 'warn',
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-])
+]);
